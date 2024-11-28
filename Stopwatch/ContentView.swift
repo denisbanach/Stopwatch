ffb6c8dbaaec649bae1c3711a7aa4e4493711cf2
Denis Banach <denis.banach@gmail.com>
2024-11-28 19:51:00 +0100

    Update ContentView.swift

--- Stopwatch/ContentView.swift
+++ Stopwatch/ContentView.swift
@@ -8,15 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var timer:Timer?
+    @State private var elapsedSeconds:Int = 0
+    @State private var timerIsRunning:Bool = false
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            
+            Text("\(elapsedSeconds)")
+                .fontWeight(.bold)
+                .fontDesign(.monospaced)
+                .font(.custom("Arial",size: 40))
+            
+            
+            Button {
+                if(timerIsRunning == false) {
+                    startTimer()
+                    timerIsRunning = true
+                }else{
+                    stopTimer()
+                    timerIsRunning = false
+                }
+            } label: {
+                Text(getStartButtonText())
+            }
+
+            
+            }
+        
+        
         .padding()
     }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
+            elapsedSeconds+=1
+        })
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    private func getStartButtonText() -> String {
+        var startButtonText = ""
+        
+        if(timerIsRunning == false) {
+            startButtonText = "Start"
+        }else{
+            startButtonText = "Stop"
+        }
+        
+        return startButtonText
+    }
+    
 }
 
 #Preview {
