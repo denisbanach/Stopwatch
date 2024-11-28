9fb278202dc52cec57fc2e1d46412c7d885991a4
Denis Banach <denis.banach@gmail.com>
2024-11-28 17:45:45 +0100

    init

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Denis Banach on 28.11.24.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
